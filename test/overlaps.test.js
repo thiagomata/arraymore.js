a1e41d90fe2615e1e8df7f8f09cd09cb91ad15ca
Thiago Mata <thiago.henrique.mata@gmail.com>
2018-03-10 12:07:46 +1300

    fixing the test title

--- test/overlaps.test.js
+++ test/overlaps.test.js
@@ -1,7 +1,7 @@
 const List = require('../index');
 describe('overlaps', () => {
 
-  it('overlaps of [1,2,3,4,5,6] => [1,2,3,4,5]', () => {
+  it('create the overlaps from data object', () => {
     var years = [1,2,3,4,5,6,7];
     var data = [{x:10,y:100},{x:3,y:30},{x:5,y:50},{x:11,y:110}]
     var expected = [0,0,30,0,50,0,0]
